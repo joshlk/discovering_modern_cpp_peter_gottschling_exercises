2c869329f1b36d4fe5c16f65b0a6ed66687b50f6
Josh Levy-Kramer <josh@outra.co.uk>
2019-01-23 10:37:16 +0000

    Work on chapter1 ex2

---
+++ chapter_1/ex2_arrays_and_pointers/1_declarations.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Josh Levy-Kramer on 2019-01-22.
+//
+
+int main() {
+    // pointer to a character
+    char* c = new char;
+
+    // array of 10 integers
+    int x[10];
+
+    // pointer to an array of 10 integers
+    int* y = new int[10];
+
+    // pointer to an array of character strings
+    char* k = new char[4];
+
+    // pointer to pointer to a character
+    char* c2 = c;
+
+    // integer constant
+    const int i = 10;
+
+    // pointer to an integer constant
+    //int* j = 10;
+
+    // constant pointer to an integer
+    //int* const b = 10; // shrug
+
+    return 0;
+}
---
+++ chapter_1/ex2_arrays_and_pointers/2_stack.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Josh Levy-Kramer on 2019-01-22.
+//
+
+// Make a small program that creates arrays on the stack (fixed-size arrays) and arrays on the heap (using allocation).
+// Use valgrind to check what happens when you do not delete them correctly.
+
+int main() {
+
+}
